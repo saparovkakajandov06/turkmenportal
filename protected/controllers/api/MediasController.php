ad6308268f42e7d8c01f63e4c3f5e9b16adebe58
ecmngnt <ecmngnt@gmail.com>
2020-11-24 13:28:02 +0500

    api medias controller added top action and some changes

--- protected/controllers/api/MediasController.php
+++ protected/controllers/api/MediasController.php
@@ -45,15 +45,15 @@ class MediasController extends Controller
             $mainDoc = $model->getDocument();
             if (isset($mainDoc) && $mainDoc->getVideoPath()) {
                 $extra = [];
-                $image_url = $mainDoc->resize(512, 288, 'crop', false, false);
-                $thumb_url = $mainDoc->resize(100, 100, 'crop', false, false);
+                $image_url = $mainDoc->resize(512, 288, 'w', false, false);
+                $thumb_url = $mainDoc->resize(100, 100, 'w', false, false);
                 $videoPath = $mainDoc->getVideoUrl();
                 $type = 'video/mp4';
                 $extra = [
                     'type' => $type,
                     'thumb_url' => 'https://turkmenportal.com'.$thumb_url,
-//                    'image_url' => 'https://turkmenportal.com'.$image_url,
-//                    'video_url' => 'https://turkmenportal.com'.$videoPath,
+                    'image_url' => 'https://turkmenportal.com'.$image_url,
+                    'video_url' => 'https://turkmenportal.com'.$videoPath,
 
                 ];
             } else {
@@ -62,21 +62,21 @@ class MediasController extends Controller
                 unset($thumb_url);
                 unset($images);
                 if (isset($mainDoc)){
-                    $image_url = 'https://turkmenprtal.com'.$mainDoc->resize(512, 288, 'crop', false, false);
-                    $thumb_url = 'https://turkmenportal.com'.$mainDoc->resize(100, 100, 'crop', false, false);
+                    $image_url = 'https://turkmenprtal.com'.$mainDoc->resize(512, 288, 'w', false, false);
+                    $thumb_url = 'https://turkmenportal.com'.$mainDoc->resize(100, 100, 'w', false, false);
                 }
                 $mainDoc = $model->documents();
                 if (count($mainDoc) > 1){
                     $images = [];
                     foreach ($mainDoc as $doc){
-                        $images[] = 'https://turkmenportal.com'.$doc->resize(512, 288, 'crop', false, false);
+                        $images[] = 'https://turkmenportal.com'.$doc->resize(512, 288, 'w', false, false);
                     }
                 }
                 $extra = [
                     'type' => 'image',
                     'thumb_url' => 'https://turkmenportal.com'.$thumb_url,
-//                    'image_url' => 'https://turkmenportal.com'.$image_url,
-//                    'image_urls' =>'https://turkmenportal.com'.$images,
+                    'image_url' => 'https://turkmenportal.com'.$image_url,
+                    'image_urls' =>'https://turkmenportal.com'.$images,
 
                 ];
             }
@@ -98,6 +98,75 @@ class MediasController extends Controller
     }
 
 
+    public function actionTop()
+    {
+        $cat_id = 338;
+        if (isset($_GET['hl'])){
+            if ($_GET['hl'] == 'tm' || $_GET['hl'] == 'ru' || $_GET['hl'] == 'en')
+                $hl = $_GET['hl'];
+        } else {
+            $hl = 'ru';
+        }
+        if (isset($_GET['per_page']))
+            $per_page = (int)$_GET['per_page'];
+        if (!isset($per_page)){
+            $per_page = 6;
+        }
+        yii::app()->language = $hl;
+
+        $modelBlog = new BlogWrapper('search');
+        $modelCategory = Category::model()->findByPk($cat_id);
+        $modelBlog->default_scope = array('enabled','sort_trend_asc');
+
+        if (isset($modelCategory) && isset($modelCategory->parent_id) && $modelCategory->parent_id > 0){
+            $modelBlog->category_id = $modelCategory->id;
+        }
+        elseif (isset ($modelCategory) && ($modelCategory->parent_id == null || $modelCategory->parent_id == 0))
+            $modelBlog->parent_category_id = $modelCategory->id;
+        $dataProvider = $modelBlog->apiSearchForCategory(100, 0);
+        $models = $dataProvider->getData();
+
+
+        foreach ($models as $key => $model){
+
+            $mainDoc = $model->getDocument();
+            if (isset($mainDoc) && $mainDoc->getVideoPath() && $per_page > 0) {
+                $per_page --;
+                $extra = [];
+                $image_url = $mainDoc->resize(512, 288, 'w', false, false);
+                $thumb_url = $mainDoc->resize(100, 100, 'w', false, false);
+                $videoPath = $mainDoc->getVideoUrl();
+                $type = 'video/mp4';
+                $extra = [
+                    'type' => $type,
+                    'thumb_url' => 'https://turkmenportal.com'.$thumb_url,
+                    'image_url' => 'https://turkmenportal.com'.$image_url,
+                    'video_url' => 'https://turkmenportal.com'.$videoPath,
+
+                ];
+                $data = array(
+                    'id' => (int)$model->id,
+                    'title' => $model->getTitle(),
+//                'content' => $model->getText(),
+                    'date' => $model->date_added,
+                    'cat_name' => $model->category->name,
+                    'cat_id' => (int)$model->category->id,
+                    'view_count' => (int)$model->visited_count,
+                    'url' => $model->createAbsoluteUrl(),
+                );
+                $result['models'][] =array_merge($data,$extra);
+            }
+
+        }
+
+
+        if (!isset($data)){
+            $data['models'] = [];
+        }
+        header('Content-Type: application/json; charset=UTF-8');
+        echo Json::encode($data);die;
+    }
+
     public function actionView()
     {
         if (isset($_GET['id']))
@@ -116,8 +185,8 @@ class MediasController extends Controller
             $mainDoc = $model->getDocument();
             if (isset($mainDoc) && $mainDoc->getVideoPath()) {
                 $extra = [];
-                $image_url = $mainDoc->resize(512, 288, 'crop', false, false);
-                $thumb_url = $mainDoc->resize(100, 100, 'crop', false, false);
+                $image_url = $mainDoc->resize(512, 288, 'w', false, false);
+                $thumb_url = $mainDoc->resize(100, 100, 'w', false, false);
                 $videoPath = $mainDoc->getVideoUrl();
                 $type = 'video/mp4';
                 $extra = [
@@ -133,14 +202,14 @@ class MediasController extends Controller
                 unset($thumb_url);
                 unset($images);
                 if (isset($mainDoc)){
-                    $image_url = 'https://turkmenportal.com'.$mainDoc->resize(512, 288, 'crop', false, false);
-                    $thumb_url = 'https://turkmenportal.com'.$mainDoc->resize(100, 100, 'crop', false, false);
+                    $image_url = 'https://turkmenportal.com'.$mainDoc->resize(512, 288, 'w', false, false);
+                    $thumb_url = 'https://turkmenportal.com'.$mainDoc->resize(100, 100, 'w', false, false);
                 }
                 $mainDoc = $model->documents();
                 if (count($mainDoc) > 1){
                     $images = [];
                     foreach ($mainDoc as $doc){
-                        $images[] = 'https://turkmenportal.com'.$doc->resize(512, 288, 'crop', false, false);
+                        $images[] = 'https://turkmenportal.com'.$doc->resize(512, 288, 'w', false, false);
                     }
                 }
                 $extra = [
