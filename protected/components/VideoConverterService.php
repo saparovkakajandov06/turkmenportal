9e7739a21755ba0e346e6b094238a81e11c94e43
ecmngnt <ecmngnt@gmail.com>
2022-05-07 16:38:46 +0500

    changed convertor api google play & app store icons resized

--- protected/components/VideoConverterService.php
+++ protected/components/VideoConverterService.php
@@ -107,7 +107,7 @@ class VideoConverterService
             $curl = curl_init();
             curl_setopt_array($curl, array(
 //                CURLOPT_URL => "https://nesipetsin.info/payment/backend/api/video/convert",
-                CURLOPT_URL => "http://mygreenway.website/backend/tk/convertor/convertor",
+                CURLOPT_URL => "http://217.174.227.211/backend/convertor/convertor",
                 CURLOPT_RETURNTRANSFER => true,
                 CURLOPT_ENCODING => "",
                 CURLOPT_MAXREDIRS => 10,
